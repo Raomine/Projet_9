ca8986b119b03a6703c2b782d425fc109c0d6031
Hicham Zarour <zarour.hicham@gmail.com>
2023-11-17 18:51:18 +0100

    Ajout du loading lazy sur les img / Debut du changement du code en vue de la supression de botstrap

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,41 @@ $(document).ready(function () {
     tagsPosition: "top",
   });
 });
+
+const slides = [
+  {
+    image: "wedding_photo.webp",
+  },
+  {
+    image: "audience_in_a_concert.webp",
+  },
+  {
+    image: "pedestrian_crossing.webp",
+  },
+];
+
+let arrowLeft = document.querySelector(".carousel-control-prev");
+let arrowRight = document.querySelector(".carousel-control-next");
+let dots = document.querySelector(".carousel-indicators");
+let bannerImage = document.querySelector(".banner_img");
+let img = 0;
+let dotSelected = document.querySelectorAll(".dot");
+dotSelected[img].classList.add("dot_selected");
+
+arrowLeft.addEventListener("click", () => {
+  console.log("Clic sur la flèche de gauche");
+  dotSelected[img].classList.remove("dot_selected");
+  img = (img - 1 + slides.length) % slides.length;
+
+  bannerImage.src = "./assets/images/slider/" + slides[img].image;
+  dotSelected[img].classList.add("dot_selected");
+});
+
+arrowRight.addEventListener("click", () => {
+  console.log("Clic sur la flèche de droite");
+  dotSelected[img].classList.remove("dot_selected");
+  img = (img + 1) % slides.length;
+
+  bannerImage.src = "./assets/images/slider/" + slides[img].image;
+  dotSelected[img].classList.add("dot_selected");
+});
