e109ff951726856fb3137f5121b5e8e85bc6ff2a
Hicham Zarour <zarour.hicham@gmail.com>
2023-11-27 12:41:25 +0100

    Modification des microformats / Correction au niveau de la galerie

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,7 +15,6 @@ const slides = [
     source: "pedestrian_crossing_mobile.webp",
   },
 ];
-
 let arrowLeft = document.querySelector(".carousel-control-prev");
 let arrowRight = document.querySelector(".carousel-control-next");
 let dots = document.querySelector(".carousel-indicators");
@@ -25,50 +24,40 @@ let bannerSourceTwo = document.querySelector(".source_two");
 let img = 0;
 let dotSelected = document.querySelectorAll(".dot");
 dotSelected[img].classList.add("dot_selected");
-
 arrowLeft.addEventListener("click", () => {
   console.log("Clic sur la flèche de gauche");
   dotSelected[img].classList.remove("dot_selected");
   img = (img - 1 + slides.length) % slides.length;
-
   bannerImage.src = "./assets/images/slider/" + slides[img].image;
   bannerImage.alt = slides[img].tag;
   bannerSource.srcset = "./assets/images/slider/" + slides[img].source;
   bannerSourceTwo.srcset = "./assets/images/slider/" + slides[img].image;
   dotSelected[img].classList.add("dot_selected");
 });
-
 arrowRight.addEventListener("click", () => {
   console.log("Clic sur la flèche de droite");
   dotSelected[img].classList.remove("dot_selected");
   img = (img + 1) % slides.length;
-
   bannerImage.src = "./assets/images/slider/" + slides[img].image;
   bannerImage.alt = slides[img].tag;
   bannerSource.srcset = "./assets/images/slider/" + slides[img].source;
   bannerSourceTwo.srcset = "./assets/images/slider/" + slides[img].image;
   dotSelected[img].classList.add("dot_selected");
 });
-
 function getCategories() {
   const filters = document.querySelectorAll("#filters div");
-
   filters.forEach((filter) => {
     filter.addEventListener("click", function () {
       const tag = this.id;
-
-      const images = document.querySelectorAll(".gallery img");
-
-      images.forEach((image) => {
+      const picture = document.querySelectorAll(".gallery .gallery_img");
+      picture.forEach((image) => {
         image.classList.replace("active", "inactive");
-
         if (tag in image.dataset || tag === "all") {
           image.classList.replace("inactive", "active");
         }
       });
     });
   });
-
   filters.forEach((element) => {
     element.addEventListener("click", () => {
       filters.forEach((element) => {
@@ -78,44 +67,35 @@ function getCategories() {
     });
   });
 }
-
 window.addEventListener("DOMContentLoaded", getCategories);
-
 const modal = document.querySelector(".modal");
 const activate = document.querySelectorAll(".activate");
-
 activate.forEach((activate) =>
   activate.addEventListener("click", openAndCloseModal)
 );
 function openAndCloseModal() {
   modal.classList.toggle("active");
 }
-
-const gallery = document.querySelectorAll(".gallery .gallery_img ");
+const gallery = document.querySelectorAll(".gallery img");
 const modalImg = document.querySelector(".modal_img");
-
 window.onload = () => {
   for (let i = 0; i < gallery.length; i++) {
     let index = i;
-    gallery[i].onclick = () => {
+    gallery[index].onclick = () => {
       function preview() {
         let selectedImgUrl = gallery[index].src;
         modalImg.src = selectedImgUrl;
       }
-
       const leftArrow = document.querySelector(".arrow_left");
       const righttArrow = document.querySelector(".arrow_right");
-
       leftArrow.onclick = () => {
         index = (index - 1 + gallery.length) % gallery.length;
         preview();
       };
-
       righttArrow.onclick = () => {
         index = (index + 1) % gallery.length;
         preview();
       };
-
       preview();
     };
   }
